8325385f447bc722271427ce0d54de2ed051ad2e
Vaibhav-afk <vbhvka@gmail.com>
2022-06-09 17:46:33 +0530

    featured done

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -22,38 +22,31 @@ export default function Featured() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper} style={{transform: `translateX(${-100*index}vw)`}}>
-        <div
-          className={styles.arrowContainer}
-          style={{ left: 0 }}
-          onClick={() => handleArrow("left")}
-        >
-          <Image
-            src="/img/arrowl.png"
-            alt=""
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("left")}
+      >
+        <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain" />
+      </div>
 
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translateX(${-100 * index}vw)` }}
+      >
         {images.map((img, i) => (
           <div className={styles.imgContainer} key={i}>
             <Image src={img} alt="" layout="fill" objectFit="contain" />
           </div>
         ))}
-        
-        <div
-          className={styles.arrowContainer}
-          style={{ right: 0 }}
-          onClick={() => handleArrow("right")}
-        >
-          <Image
-            src="/img/arrowr.png"
-            alt=""
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
+      </div>
+
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("right")}
+      >
+        <Image src="/img/arrowr.png" alt="" layout="fill" objectFit="contain" />
       </div>
     </div>
   );
